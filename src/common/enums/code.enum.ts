0f2e788b06136803981d90acd9c343da5022b4bd
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-04 23:06:35 +0900

    [#3] Code Enum 추가

--- src/common/enums/code.enum.ts
+++ src/common/enums/code.enum.ts
@@ -2,6 +2,11 @@ export enum CertificationMethodCode {}
 export enum MarketingConsentCode {}
 export enum PrivacyConsentCode {}
 export enum VideoOptionCode {}
+
 export enum RefVideoDifficultyCode {}
 export enum RefVideoSourceCode {}
 export enum PracticeVideoTypeCode {}
+
+export enum GradeCode {}
+
+export enum IsBookmarkedCode {}
