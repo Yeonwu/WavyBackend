af7c48e1000f58643a4ea19b48be0ac8a4fc98d1
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-04 23:33:58 +0900

    [#3] MemberRefVideo 엔티티 칼럼 타입, PK 추가

--- src/members-ref-videoes/entities/members-ref-videoes.entity.ts
+++ src/members-ref-videoes/entities/members-ref-videoes.entity.ts
@@ -1,4 +1,4 @@
-import { IsEnum } from 'class-validator';
+import { IsEnum, IsNumberString } from 'class-validator';
 import { MemberRefVideoIsBookmarkedCode } from 'src/common/enums/code.enum';
 import { Member } from 'src/members/entities/members.entity';
 import { RefVideo } from 'src/ref-videoes/entities/ref-video.entity';
@@ -7,8 +7,9 @@ import { CoreEntity } from '../../common/entities/core.entity';
 
 @Entity()
 export class MemberRefVideo extends CoreEntity {
-    @PrimaryGeneratedColumn()
-    key: number;
+    @PrimaryGeneratedColumn({ name: 'mbr_rv_seq', type: 'bigint' })
+    @IsNumberString()
+    mbrRvSeq: string;
 
     @ManyToOne((type) => Member, (member) => member.memberRefVideoes)
     member: Member;
@@ -16,7 +17,7 @@ export class MemberRefVideo extends CoreEntity {
     @ManyToOne((type) => RefVideo, (refVideo) => refVideo.memberRefVideoes)
     refVideo: RefVideo;
 
-    @Column({ name: 'is_bookmarked' })
+    @Column({ name: 'is_bookmarked', type: 'varchar', length: 50 })
     @IsEnum(MemberRefVideoIsBookmarkedCode)
     isBookMarkedCode: string;
 }
