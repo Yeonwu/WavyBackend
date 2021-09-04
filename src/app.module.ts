09ea6257923284b493d3e7d19cfadd65522d48c0
haeseoklee <haeseoklee.dev@gmail.com>
2021-09-04 04:22:05 +0000

    [#2] nest 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MembersModule } from './members/members.module';
+import { PracticesModule } from './practices/practices.module';
+import { AnalysesModule } from './analyses/analyses.module';
+import { RefVideoesModule } from './ref-videoes/ref-videoes.module';
+import { MembersRefVideoesModule } from './members-ref-videoes/members-ref-videoes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MembersModule, PracticesModule, AnalysesModule, RefVideoesModule, MembersRefVideoesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
