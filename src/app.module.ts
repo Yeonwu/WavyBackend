833b9adc0ee09fd1eb3c8dbe6e41d30ffe10b78a
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-04 16:19:33 +0900

    [#3] TypeORM 환경 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,45 @@ import { PracticesModule } from './practices/practices.module';
 import { AnalysesModule } from './analyses/analyses.module';
 import { RefVideoesModule } from './ref-videoes/ref-videoes.module';
 import { MembersRefVideoesModule } from './members-ref-videoes/members-ref-videoes.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [MembersModule, PracticesModule, AnalysesModule, RefVideoesModule, MembersRefVideoesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.dev',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'test').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USER: Joi.string().required(),
+        DB_PW: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        PRIVATE_KEY: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
+      entities: [],
+    }),
+    MembersModule,
+    PracticesModule,
+    AnalysesModule,
+    RefVideoesModule,
+    MembersRefVideoesModule,
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
