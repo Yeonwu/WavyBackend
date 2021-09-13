3ad6da64ac96187fd02c40e2a259ee5f0f82758c
haeseoklee <haeseoklee.dev@gmail.com>
2021-09-13 20:45:12 +0900

    [#11] tag 엔티티와 refvideo 엔티티를 ManyToMany로 조인함

---
+++ src/ref-videos/entities/ref-video.entity.ts
@@ -0,0 +1,83 @@
+import { IsEnum, IsNumberString, IsString, IsUrl } from 'class-validator';
+import { Analysis } from 'src/analyses/entities/analyses.entity';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import {
+    RefVideoDifficultyCode,
+    RefVideoSourceCode,
+} from 'src/common/enums/code.enum';
+import { MemberRefVideo } from 'src/members-ref-videoes/entities/members-ref-videoes.entity';
+import { Practice } from 'src/practices/entities/practice.entity';
+import {
+    Column,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    OneToMany,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Tag } from './tag.entity';
+
+@Entity()
+export class RefVideo extends CoreEntity {
+    @PrimaryGeneratedColumn({ name: 'rv_seq', type: 'bigint' })
+    @IsNumberString()
+    rvSeq: string;
+
+    @Column({ name: 'rv_source', type: 'varchar', length: 50 })
+    @IsEnum(RefVideoSourceCode)
+    rvSource: string;
+
+    @Column({ name: 'rv_source_title', type: 'varchar', length: 255 })
+    @IsString()
+    rvSourceTitle: string;
+
+    @Column({ name: 'rv_source_account_name', type: 'varchar', length: 255 })
+    @IsString()
+    rvSourceAccountName: string;
+
+    @Column({ name: 'rv_url', nullable: true, type: 'varchar', length: 255 })
+    @IsUrl()
+    rvUrl?: string;
+
+    @Column({ name: 'rv_duration', type: 'time' })
+    @IsString()
+    rvDuration: string;
+
+    @Column({ name: 'rv_difficulty', type: 'varchar', length: 50 })
+    @IsEnum(RefVideoDifficultyCode)
+    rvDifficulty: string;
+
+    @Column({
+        name: 'rv_song_name',
+        nullable: true,
+        type: 'varchar',
+        length: 255,
+    })
+    @IsString()
+    rvSongName?: string;
+
+    @Column({
+        name: 'rv_artist_name',
+        nullable: true,
+        type: 'varchar',
+        length: 255,
+    })
+    @IsString()
+    rvArtistName?: string;
+
+    @OneToMany((type) => Analysis, (analysis) => analysis.refVideo)
+    analyses: Analysis[];
+
+    @OneToMany((type) => Practice, (practice) => practice.refVideo)
+    practices: Practice[];
+
+    @OneToMany(
+        (type) => MemberRefVideo,
+        (memberRefVideo) => memberRefVideo.refVideo,
+    )
+    memberRefVideoes: MemberRefVideo[];
+
+    @ManyToMany((type) => Tag, (tag) => tag.refVideos)
+    @JoinTable()
+    tags: Tag[];
+}
