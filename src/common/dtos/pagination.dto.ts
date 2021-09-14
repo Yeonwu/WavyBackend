668e798b71013b8f7e530ed1c8b78c3ee2700dd5
haeseoklee <haeseoklee.dev@gmail.com>
2021-09-14 22:34:13 +0900

    [#20] Pagination 에 take와 skip 구현

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -4,6 +4,10 @@ import { CoreOutput } from './output.dto';
 export class PaginationInput {
     @IsNumber()
     page: number;
+
+    static take = 9;
+    static skip: (page: number) => number = (page) =>
+        (page - 1) * PaginationInput.take;
 }
 
 export class PaginationOutput extends CoreOutput {
