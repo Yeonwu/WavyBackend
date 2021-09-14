57ad3efd5b2fddeb418d32fac05c0ac91084ade5
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-14 11:31:42 +0900

    [#19] CreateMemberInput DTO에 mbrEmail 칼럼 추가

--- src/members/dtos/create-member.dto.ts
+++ src/members/dtos/create-member.dto.ts
@@ -2,6 +2,7 @@ import { Member } from '../entities/members.entity';
 import { ApiProperty, ApiPropertyOptional, PickType } from '@nestjs/swagger';
 
 export class CreateMemberInput extends PickType(Member, [
+    'mbrEmail',
     'mbrNickname',
     'certificationMethodCode',
     'profileImageUrl',
