96fb854418b1742b6725c514e0c89a6a3796e75b
haeseoklee <haeseoklee.dev@gmail.com>
2021-09-17 00:53:26 +0900

    [#14] Common 모듈에 Swagger 적용

--- src/common/dtos/output.dto.ts
+++ src/common/dtos/output.dto.ts
@@ -1,9 +1,13 @@
-import { IsBoolean, IsString } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsBoolean, IsOptional, IsString } from 'class-validator';
 
 export class CoreOutput {
+    @ApiPropertyOptional({ type: Number, description: '오류 메세지' })
     @IsString()
+    @IsOptional()
     error?: string;
 
+    @ApiProperty({ type: Boolean, description: '요청 성공 여부' })
     @IsBoolean()
     ok: boolean;
 }
