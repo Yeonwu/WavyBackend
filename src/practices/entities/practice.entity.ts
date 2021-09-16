bf00d3b5e46739ebd2206605ff6a922391f5c393
haeseoklee <haeseoklee.dev@gmail.com>
2021-09-17 00:53:00 +0900

    [#14] Practice 모듈에 Swagger 적용

--- src/practices/entities/practice.entity.ts
+++ src/practices/entities/practice.entity.ts
@@ -1,4 +1,11 @@
-import { IsEnum, IsNumberString, IsString, IsUrl } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+    IsEnum,
+    IsNumberString,
+    IsOptional,
+    IsString,
+    IsUrl,
+} from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { PracticeVideoTypeCode } from 'src/common/enums/code.enum';
 import { Member } from 'src/members/entities/members.entity';
@@ -14,22 +21,33 @@ import {
 
 @Entity()
 export class Practice extends CoreEntity {
+    @ApiProperty({ type: String, description: '연습 ID' })
     @PrimaryGeneratedColumn({ name: 'pt_seq', type: 'bigint' })
     @IsNumberString()
     ptSeq: string;
 
+    @ApiProperty({ type: String, description: '연습 시작시간' })
     @Column({ name: 'pt_started', type: 'timestamp' })
     @IsString()
     ptStarted: string;
 
+    @ApiProperty({ type: String, description: '연습 종료시간' })
     @Column({ name: 'pt_finished', type: 'timestamp' })
     @IsString()
     ptFinished: string;
 
+    @ApiProperty({
+        enum: PracticeVideoTypeCode,
+        description: '연습에 사용된 영상의 타입(학습용 영상 또는 유튜브 영상)',
+    })
     @Column({ name: 'pt_video_type_cd', type: 'varchar', length: 50 })
     @IsEnum(PracticeVideoTypeCode)
     ptVideoTypeCode: string;
 
+    @ApiPropertyOptional({
+        type: String,
+        description: '연습에 사용된 영상의 외부 URL',
+    })
     @Column({
         name: 'pt_video_url',
         nullable: true,
@@ -37,6 +55,7 @@ export class Practice extends CoreEntity {
         length: 255,
     })
     @IsUrl()
+    @IsOptional()
     ptVideoUrl?: string;
 
     @ManyToOne((type) => Member, (member) => member.practices)
