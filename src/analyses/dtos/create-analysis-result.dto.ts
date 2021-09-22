070afdfb65f4e57ad426da725f6f02245cfbd985
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-22 17:46:29 +0900

    [#34] CreateAnalysisResultInput DTO 구현

---
+++ src/analyses/dtos/create-analysis-result.dto.ts
@@ -0,0 +1,11 @@
+import { OmitType } from '@nestjs/swagger';
+import { Analysis } from '../entities/analyses.entity';
+
+export class CreateAnalysisResultInput extends OmitType(Analysis, [
+    'createdDate',
+    'updatedDate',
+    'creatorSeq',
+    'updaterSeq',
+    'mbrSeq',
+    'anSeq',
+]) {}
