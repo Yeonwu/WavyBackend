889acb71593016b174f962568a4fca89667e6879
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-23 00:36:33 +0900

    [#34] member 엔티티 충돌 해결

--- src/members/entities/members.entity.ts
+++ src/members/entities/members.entity.ts
@@ -18,15 +18,19 @@ import {
 } from 'src/common/enums/code.enum';
 import { MemberRefVideo } from 'src/members-ref-videoes/entities/members-ref-videoes.entity';
 import { Practice } from 'src/practices/entities/practice.entity';
+
+import { MemberExpHistory } from './mbr-exp-history.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { RefVideo } from 'src/ref-videos/entities/ref-video.entity';
 import {
     Column,
     Entity,
+    JoinTable,
+    ManyToMany,
     OneToMany,
     PrimaryGeneratedColumn,
     Unique,
 } from 'typeorm';
-import { MemberExpHistory } from './mbr-exp-history.entity';
-import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
 @Unique(['mbrKakaoSeq'])
