11026e6e9e2204a8edb56c92ad30547e1cb2f1e0
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-24 18:52:42 +0900

    [#49] DTO에 CoreOutput 적용

--- src/members/dtos/get-mbr-exp-history.ts
+++ src/members/dtos/get-mbr-exp-history.ts
@@ -1,8 +1,13 @@
 import { PickType } from '@nestjs/swagger';
+import { CoreOutput } from 'src/common/dtos/output.dto';
 import { MemberExpHistory } from '../entities/mbr-exp-history.entity';
 
-export class GetMbrExpHistoryOutput extends PickType(MemberExpHistory, [
+export class GetMbrExpHistoryEntity extends PickType(MemberExpHistory, [
     'mehExpAcc',
     'mehLevel',
     'mehTier',
 ]) {}
+
+export class GetMbrExpHistoryOutput extends CoreOutput {
+    expHistory?: GetMbrExpHistoryEntity;
+}
