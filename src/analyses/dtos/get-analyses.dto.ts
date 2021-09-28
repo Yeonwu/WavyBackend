6ee468b20b9f675ffb420737c457e6352883e807
Yeonwu Oh <yeonwu319@gmail.com>
2021-09-28 10:41:37 +0900

    [#51] 분석결과 DTO에 Swagger 적용

--- src/analyses/dtos/get-analyses.dto.ts
+++ src/analyses/dtos/get-analyses.dto.ts
@@ -1,13 +1,32 @@
-import { IsOptional } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 import { Analysis } from '../entities/analyses.entity';
 
 export class GetAnalysesOutput extends CoreOutput {
+    @ApiPropertyOptional({
+        description: '분석 결과 목록',
+        isArray: true,
+        type: Analysis,
+    })
     @IsOptional()
     analyses?: Analysis[];
 }
 
 export class GetAnalysisBySeqOutput extends CoreOutput {
+    @ApiPropertyOptional({
+        description: '분석결과',
+        type: Analysis,
+    })
     @IsOptional()
     analysis?: Analysis;
+}
+
+export class GetAnalysesBySeqInput {
+    @ApiProperty({
+        description: '분석결과 ID',
+        type: String,
+    })
+    @IsString()
+    id: string;
 }
